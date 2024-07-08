e695c7810abc504656887bbc72c9056e6793c3d2
Alexei Kruk <alexei.kruk.dev@gmail.com>
2024-07-08 21:49:10 +0300

    add owl-carousel

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,39 @@
 $(document).ready(function () {
-	$('.owl-carousel').owlCarousel();
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		margin: 10,
+		nav: false,
+		dots: true,
+		dotsEach: true,
+		// stagePadding:100,
+		// center:true,
+		// navText: [
+		// 	'<img src="img/arrow-left.svg" alt="arrow-left">',
+		// 	' <img src="img/arrow-right.svg" alt="arrow-right">',
+		// ],
+		responsive: {
+			0: {
+				items: 1,
+				navText: [],
+			},
+			768: {
+				items: 2,
+				dots: true,
+				navText: [],
+			},
+			1000: {
+				items: 4,
+			},
+		},
+	});
+
+	// Обработчик события для кнопки "Назад"
+	$('.arrows__prev').on('click', function () {
+		$('.owl-carousel').trigger('prev.owl.carousel');
+	});
+
+	// Обработчик события для кнопки "Вперед"
+	$('.arrows__next').on('click', function () {
+		$('.owl-carousel').trigger('next.owl.carousel');
+	});
 });
