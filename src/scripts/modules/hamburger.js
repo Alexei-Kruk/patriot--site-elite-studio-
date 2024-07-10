c61da418d142763b0ec5b5aa609b7d937058d038
Alexei Kruk <alexei.kruk.dev@gmail.com>
2024-07-10 20:14:21 +0300

    add hamburger

---
+++ src/scripts/modules/hamburger.js
@@ -0,0 +1,7 @@
+// var hamburger = document.querySelector('.hamburger');
+// hamburger.addEventListener('click', function () {
+// 	hamburger.classList.toggle('is-active');
+// });
+
+const burger = document.querySelector('mm-burger');
+burger.state = 'cross';
--- src/scripts/modules/menu.js
+++ src/scripts/modules/menu.js
@@ -2,30 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	new Mmenu('#menu', {
 		offCanvas: {
 			position: 'top',
-			// zposition: 'front',
-			// pageSelector: '.wrapper',
-			// pageNodetype: 'div',
 		},
-		// navbars: [
-		// 	{
-		// 		position: 'top',
-		// 		content: ['prev'],
-		// 	},
-		// ],
-		// backButton: true,
-		// theme: 'white',
-		// iconPanels: {
-		// 	add: true,
-		// 	visible: 1,
-		// },
-		// autoHeight: {
-		// 	height: true,
-		// },
-		// onClick: {
-		// 	close: true,
-		// },
 	});
-
-	// const burger = document.querySelector('mm-burger');
-	// burger.state = 'cross';
 });
