10acdf95234591bdc676c28536f425a45825009b
Alexei Kruk <alexei.kruk.dev@gmail.com>
2024-07-10 18:18:47 +0300

    change menu

--- src/scripts/modules/menu.js
+++ src/scripts/modules/menu.js
@@ -2,28 +2,28 @@ document.addEventListener('DOMContentLoaded', () => {
 	new Mmenu('#menu', {
 		offCanvas: {
 			position: 'top',
-			zposition: 'front',
-			pageSelector: '.wrapper',
-			pageNodetype: 'div',
+			// zposition: 'front',
+			// pageSelector: '.wrapper',
+			// pageNodetype: 'div',
 		},
-		navbars: [
-			{
-				position: 'top',
-				content: ['prev'],
-			},
-		],
-		backButton: true,
-		theme: 'white',
-		iconPanels: {
-			add: true,
-			visible: 1,
-		},
-		autoHeight: {
-			height: true,
-		},
-		onClick: {
-			close: true,
-		},
+		// navbars: [
+		// 	{
+		// 		position: 'top',
+		// 		content: ['prev'],
+		// 	},
+		// ],
+		// backButton: true,
+		// theme: 'white',
+		// iconPanels: {
+		// 	add: true,
+		// 	visible: 1,
+		// },
+		// autoHeight: {
+		// 	height: true,
+		// },
+		// onClick: {
+		// 	close: true,
+		// },
 	});
 
 	// const burger = document.querySelector('mm-burger');
