9d01e21a96364baabf354e0216e46392055ad555
Alexei Kruk <alexei.kruk.dev@gmail.com>
2024-07-15 19:50:27 +0300

    change src for imgs

--- src/scripts/modules/hamburger.js
+++ src/scripts/modules/hamburger.js
@@ -1,24 +1,19 @@
-// function hamburger() {
-	document.querySelector('.burger').addEventListener('click', function () {
-		var menu = document.querySelector('.menu');
-		this.classList.toggle('active');
-	
-		if (menu.classList.contains('menu__hidden')) {
-			menu.classList.remove('menu__hidden');
-			menu.classList.add('menu__fade');
-			menu.style.display = 'block';
-		} else {
-			menu.classList.add('menu__hidden');
-			menu.classList.remove('menu__fade');
-		}
-	});
-	
-	document.querySelector('.menu').addEventListener('animationend', e => {
-		if (e.animationName === 'slideUp') {
-			e.target.style.display = 'none';
-		}
-	});
-// }
+document.querySelector('.burger').addEventListener('click', function () {
+	var menu = document.querySelector('.menu');
+	this.classList.toggle('active');
 
-// export default hamburger;
+	if (menu.classList.contains('menu__hidden')) {
+		menu.classList.remove('menu__hidden');
+		menu.classList.add('menu__fade');
+		menu.style.display = 'block';
+	} else {
+		menu.classList.add('menu__hidden');
+		menu.classList.remove('menu__fade');
+	}
+});
 
+document.querySelector('.menu').addEventListener('animationend', e => {
+	if (e.animationName === 'slideUp') {
+		e.target.style.display = 'none';
+	}
+});
